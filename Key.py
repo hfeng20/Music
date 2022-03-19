fa9a1eb8fecb2519127be52a4b7324734c4ad933
Harry Feng <harryfeng@Harrys-MacBook-Pro.local>
2022-03-19 18:47:33 -0400

    basics of Key class

---
+++ Key.py
@@ -0,0 +1,11 @@
+from Chord import *
+
+class Key:
+    def __init__(self, key):
+        self.key = key
+        if len(key > 2):
+            self.root = key[:2]
+            self.quality = key[2:]
+        else:
+            self.root = key[0]
+            self.quallity = key[1]
